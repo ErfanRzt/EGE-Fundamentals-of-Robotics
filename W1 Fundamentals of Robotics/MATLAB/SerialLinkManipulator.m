a4f050fb14a24b421def91793fb451cd552e42ce
Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
2024-08-08 16:05:32 +0300

    Squashed commit of the following:
    commit 72668e8f29b5ada79e6b7124b1b69bc9f2090c81
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Thu Aug 8 15:58:15 2024 +0300
    
        ⚡️ add : Live Script for Simulations in Chapter Direct Kinematics
    
    commit 278976eee4c36ac59dd9d299c5159d5dbcd63d1f
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Wed Aug 7 18:06:12 2024 +0300
    
        :memo: docs : Add figures and examples of multiple configurations for inverse kinematics
    
    commit ce33abf62ac5df6dc70440983c832952128773c2
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Wed Aug 7 18:05:25 2024 +0300
    
        :sparkles: feat : Add Inverse Kinematics functionality to 2-DoF (RR) Planar Robot
    
    commit ec3ff8a56b4e7f7f6ff775d1132aba197441c24a
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Tue Aug 6 14:26:18 2024 +0300
    
        :bulb: docs : Add animation to demonstrate the DH algorithm
    
    commit 1d4ecf2eacba4696e15e410e055eac70e4f67289
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Tue Aug 6 14:25:35 2024 +0300
    
        :sparkles: feat : Add answers and simulation to two DH convention problem
    
    commit 4ae07f1cc36966beaae448f0a2f2039cd838d2d1
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Mon Aug 5 11:48:50 2024 +0300
    
        :fire: remove : Robotics Leg codes are now moved to a different repository
    
    commit 14f30f22c658916da34fce6899f14c9f73fc1720
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Fri Jul 26 07:38:09 2024 +0300
    
        📝 docs : Add animation of the modeled robots
    
    commit e7765ae4f1dc0e98ca3190d17a6808ab8d3d2d16
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Fri Jul 26 07:36:42 2024 +0300
    
        :sparkles: feat : Add Basic Modeling of a Robotic Leg
    
        - Homogeneous Transforms
        - Coordinate Frames
        - Plot Leg Configuration
    
    commit df4ab51bb231bc4a7b473013479a381eb9232ffb
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Wed Jul 24 14:15:19 2024 +0300
    
        :fire: Create .gitignore file
    
    commit 62f15d6c51127a05e112e8db3c8bed0966e3e6d5
    Author: Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
    Date:   Wed Jul 24 14:11:23 2024 +0300
    
        :sparkles: feat : MATLAB Script; A Class That Encompasses Parameters of a Serial Link Manipulator

---
+++ W1 Fundamentals of Robotics/MATLAB/SerialLinkManipulator.m
@@ -0,0 +1,129 @@
+classdef SerialLinkManipulator
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties
+        nJoints
+        nLinks
+        linksLength
+        initialState
+    end
+
+    methods
+        function obj = SerialLinkManipulator(links, initialState)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.nLinks = numel(links);
+            obj.linksLength = links;
+            obj.nJoints = numel(initialState);
+            obj.initialState = initialState;
+        end
+
+        function joints = taskSpace(obj, configuration, draw)
+            if nargin < 3
+                draw = false;
+            end
+
+            jointsPloar = [configuration, obj.linksLength];
+            joints = zeros([obj.nJoints + 1, 2]);
+
+            cumulativeAngle = 0;
+
+            for i = 1:obj.nJoints
+                cumulativeAngle = cumulativeAngle + jointsPloar(i, 1);
+                [x_offset, y_offset] = pol2cart(cumulativeAngle, jointsPloar(i, 2));
+            
+                joints(i+1, 1) = joints(i, 1) + x_offset;
+                joints(i+1, 2) = joints(i, 2) + y_offset;
+            end
+
+            if draw
+                figure;
+                plot(   joints(:, 1), joints(:, 2), ...
+                        "LineWidth", 5, "LineStyle", '-', "Color", 'g'  );
+                hold on;
+                plot(   joints(1:obj.nJoints, 1), joints(1:obj.nJoints, 2), ...
+                        'or', "LineWidth", 2);
+                hold on;
+                plot(   joints(1:obj.nJoints, 1), joints(1:obj.nJoints, 2), ...
+                        '.r', "LineWidth", 2);
+                hold on;
+                plot(   joints(end, 1), joints(end, 2), ...
+                        '+r', "LineWidth", 2);
+                axis equal; grid on; axis (draw*[-1 1 -1 1]);
+            end
+        end
+
+        function drawConfiguration(obj, configuration)
+            figure;
+            for i = configuration
+                joints = obj.taskSpace(i, false);
+            
+                plot1 = plot(   joints(:, 1), joints(:, 2), ...
+                        "LineWidth", 5, "LineStyle", '-', "Color", 'g'  );
+                hold on;
+                plot2 = plot(   joints(1:obj.nJoints, 1), joints(1:obj.nJoints, 2), ...
+                        'or', "LineWidth", 2);
+                hold on;
+                plot3 = plot(   joints(1:obj.nJoints, 1), joints(1:obj.nJoints, 2), ...
+                        '.r', "LineWidth", 2);
+                hold on;
+                plot4 = plot(   joints(end, 1), joints(end, 2), ...
+                        '+r', "LineWidth", 2);
+                axis equal; grid on; axis (20*[-1 1 -1 1]);
+            
+                plots = [plot1 plot2 plot3 plot4];
+                pause(0.01);
+                delete(plots);
+            end
+        end
+
+%         %MONITORING Tools for making figures and online simulation
+%         function linkRect = initLink(obj, x1, x2, y1, y2)
+%             %METHOD1 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             linkRect.vertices = [   x1  y1  ; ...
+%                                     x2  y1  ; ...
+%                                     x2  y2  ; ...
+%                                     x1  y2  ];
+% 
+%             linkRect.faces = [  1   2   3   ; ...
+%                                 1   3   4   ];
+%         end
+% 
+%         function serialLinksRect = appendLinks(obj, links, newLink)
+%             %METHOD2 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             nPrevs = size(links.vertices, 1);
+%             
+%             serialLinksRect.vertices = [links.vertices; newLink.vertices];
+%             serialLinksRect.faces = [links.faces; newLink.faces + nPrevs];
+%         end
+% 
+%         function tf = transformLinks(obj, link, rotation, translation)
+%             %METHOD3 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             tf.faces = link.faces;
+%             
+%             c = cosd(rotation);
+%             s = sind(rotation);
+%             
+%             tf.vertices = link.vertices * [c s; -s c] + translation;
+%         end
+% 
+%         function links = serialLinkPoly(obj, configuration)
+%             %METHOD4 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             h = min(obj.linksLength) / 20;
+% 
+%             a = obj.initLink(0, obj.linksLength(1), -h, h);
+%             links = a;
+%             
+%             for i = 1:(obj.nLinks-1)
+%                 links = obj.appendLinks(a, obj.transformLinks(links, configuration(i), [ obj.linksLength(i), 0]));
+%             end
+%             
+%             links = obj.transformLinks(links, configuration(end), [0 0]);
+%         end
+    end
+end
