7f0f09f2817f6dfb558b593c28683cab47ab38a1
Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
2024-08-15 11:18:43 +0300

    :pencil2: docs : Add comments and procedures to Link.m

--- MATLAB/Link.m
+++ MATLAB/Link.m
@@ -1,4 +1,22 @@
 classdef Link < matlab.mixin.Copyable
+    % LINK Class representing a single rigid link in a serial manipulator.
+    % 
+    % This class defines a rigid link object characterized by its
+    % Denavit-Hartenberg (DH) parameters, dynamic properties such as mass,
+    % center of mass, and inertia tensor. It also supports copying the object 
+    % to create independent instances.
+    %
+    % Properties:
+    %   name  - Name of the link (default: 'NewLink')
+    %   type  - Type of the joint, either 'Revolute' or 'Prismatic' (default: 'Revolute')
+    %   dh    - Denavit-Hartenberg parameters [theta, d, a, alpha] (default: [0, 0, 0, 0])
+    %   m     - Mass of the link (default: 0)
+    %   r     - Center of mass in the local frame (3x1 vector) (default: [0; 0; 0])
+    %   I     - Inertia tensor in the local frame (3x3 matrix) (default: zeros(3,3))
+    %
+    % Methods:
+    %   Link  - Constructor method for initializing the Link object.
+
     properties
         name        % Name of the link
         type        % Type of the joint: 'Revolute' or 'Prismatic'
@@ -9,27 +27,48 @@ classdef Link < matlab.mixin.Copyable
     end
 
     properties (Access=private, Constant)
-        defaultName = 'NewLink';
-        defaultType = 'Revolute';
-        defaultM = 0;
-        defaultR = [0; 0; 0];
-        defaultI = zeros(3, 3);  % Inertia matrix initialized to zero
-        defaultDH = [0, 0, 0, 0];  % Default DH parameters
+        defaultName = 'NewLink';          % Default name for the link
+        defaultType = 'Revolute';         % Default joint type
+        defaultM = 0;                     % Default mass
+        defaultR = [0; 0; 0];             % Default center of mass
+        defaultI = zeros(3, 3);           % Default inertia tensor
+        defaultDH = [0, 0, 0, 0];         % Default DH parameters
     end
     
     methods
         function obj = Link(dhparams, varargin)
-            % Constructor method for the Link class
-            % dhparams - Denavit-Hartenberg parameters (positional argument)
+            % LINK Constructor for creating a Link object.
+            %
+            % Syntax:
+            %   obj = Link(dhparams)
+            %   obj = Link(dhparams, 'PropertyName', PropertyValue, ...)
+            %
+            % Description:
+            %   Constructs a Link object with specified Denavit-Hartenberg (DH) 
+            %   parameters and optional dynamic properties.
+            %
+            % Input Arguments:
+            %   dhparams - (optional) DH parameters as a 1x4 vector [theta, d, a, alpha].
+            %              If not provided, default is [0, 0, 0, 0].
+            %
+            %   Name-Value Pair Arguments:
+            %     'Name' - (optional) Name of the link (default: 'NewLink').
+            %     'Type' - (optional) Type of joint: 'Revolute' or 'Prismatic' (default: 'Revolute').
+            %     'm'    - (optional) Mass of the link (default: 0).
+            %     'r'    - (optional) Center of mass as a 3x1 vector (default: [0; 0; 0]).
+            %     'I'    - (optional) Inertia tensor as a 3x3 matrix (default: zeros(3,3)).
+            %
+            % Output:
+            %   obj - Instance of the Link class.
 
-            % Check if DH parameters are provided, if not, use default
+            % Check if DH parameters are provided; otherwise, use default
             if nargin < 1 || isempty(dhparams)
                 obj.dh = obj.defaultDH;
             else
                 obj.dh = dhparams;
             end
 
-            % Parse the name-value pair arguments for other properties
+            % Parse optional name-value pair arguments for other properties
             parser = inputParser;
             addParameter(parser, 'Name', obj.defaultName, @(x) ischar(x) || isstring(x));
             addParameter(parser, 'Type', obj.defaultType, @(x) ischar(x) || isstring(x));
