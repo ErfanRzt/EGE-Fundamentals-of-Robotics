50642e91f14f51bd03ff381a8a343928f8e74002
Erfan Riazati <64183227+ErfanRzt@users.noreply.github.com>
2024-08-21 16:03:10 +0300

    :bulb: docs : Add comments for the newly updated Link object usage

--- MATLAB/PrismaticLink.m
+++ MATLAB/PrismaticLink.m
@@ -1,12 +1,35 @@
 classdef PrismaticLink < Link
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
-
     methods
-        function P = PrismaticLink(theta, a, alpha, varargin)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            P = P@Link([theta, 0, a, alpha], 'type', 'prismatic', varargin{:});
+        function P = PrismaticLink(theta, a, alpha, offset, varargin)
+            % PRISMATICLINK Construct an instance of this class.
+            %
+            % Syntax:
+            %   obj = PrismaticLink(theta, a, alpha, offset)
+            %   obj = PrismaticLink(theta, a, alpha, offset, 'PropertyName', PropertyValue, ...)
+            %
+            % Description:
+            %   Creates a PrismaticLink object using specified Denavit-Hartenberg (DH) 
+            %   parameters for a prismatic joint along with optional dynamic properties.
+            %
+            % Input Arguments:
+            %   theta   - [scalar] Joint rotation angle
+            %   a       - [scalar] Link length
+            %   alpha   - [scalar] Link twist
+            %   offset  - [scalar] Link perpendicular distance offset
+            %
+            %   Name-Value Pair Arguments:
+            %     'Name'    - [char or string]  Name of the link (default: 'NewLink').
+            %   * 'Type'    - [char or string]  (WARNING) DO NOT MODIFY! Filled internally as 'Prismatic'.
+            %     'qlim'    - [1x2 array]       Joint limits [min, max] (default: [-inf, inf]).
+            %     'm'       - [scalar]          Mass of the link (default: 0).
+            %     'r'       - [3x1 vector]      Center of mass (default: [0; 0; 0]).
+            %     'I'       - [3x3 matrix]      Inertia tensor (default: zeros(3,3)).
+            %
+            % Output:
+            %   obj     - Instance of the PrismaticLink class.
+            
+            % Call the parent class constructor with specified parameters.
+            P = P@Link([theta, offset, a, alpha], 'type', 'Prismatic', varargin{:});
         end
     end
 end
--- MATLAB/RevoluteLink.m
+++ MATLAB/RevoluteLink.m
@@ -1,12 +1,35 @@
 classdef RevoluteLink < Link
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
-
     methods
-        function R = RevoluteLink(d, a, alpha, varargin)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            R = R@Link([0, d, a, alpha], 'type', 'revolute', varargin{:});
+        function R = RevoluteLink(d, a, alpha, offset, varargin)
+            % REVOLUTELINK Construct an instance of this class.
+            %
+            % Syntax:
+            %   obj = RevoluteLink(d, a, alpha, offset)
+            %   obj = RevoluteLink(d, a, alpha, offset, 'PropertyName', PropertyValue, ...)
+            %
+            % Description:
+            %   Creates a RevoluteLink object using specified Denavit-Hartenberg (DH) 
+            %   parameters for a revolute joint along with optional dynamic properties.
+            %
+            % Input Arguments:
+            %   d       - [scalar] Link perpendicular distance
+            %   a       - [scalar] Link length
+            %   alpha   - [scalar] Link twist
+            %   offset  - [scalar] Joint rotation angle offset
+            %
+            %   Name-Value Pair Arguments:
+            %     'Name'    - [char or string]  Name of the link (default: 'NewLink').
+            %   * 'Type'    - [char or string]  (WARNING) DO NOT MODIFY! Filled internally as 'Revolute'.
+            %     'qlim'    - [1x2 array]       Joint limits [min, max] (default: [-inf, inf]).
+            %     'm'       - [scalar]          Mass of the link (default: 0).
+            %     'r'       - [3x1 vector]      Center of mass (default: [0; 0; 0]).
+            %     'I'       - [3x3 matrix]      Inertia tensor (default: zeros(3,3)).
+            %
+            % Output:
+            %   obj     - Instance of the RevoluteLink class.
+            
+            % Call the parent class constructor with specified parameters.
+            R = R@Link([offset, d, a, alpha], 'type', 'Revolute', varargin{:});
         end
     end
 end
